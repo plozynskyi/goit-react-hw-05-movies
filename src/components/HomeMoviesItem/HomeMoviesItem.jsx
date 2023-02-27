42f1f285d00350e9ba525a160fbf9a2ed625dea3
Павел <p.lozynskyi@gmail.com>
2023-02-28 00:34:22 +0200

    Update HomeMoviesItem.jsx

--- src/components/HomeMoviesItem/HomeMoviesItem.jsx
+++ src/components/HomeMoviesItem/HomeMoviesItem.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 import { useLocation } from 'react-router-dom';
-
+import { routes } from '../../routes';
 import {
   MovieLink,
   MoviesItem,
@@ -11,15 +11,13 @@ import {
 
 const HomeMoviesItem = ({ title, name, id, poster }) => {
   const location = useLocation();
-  console.log(location);
-
   const imageUrl = 'https://image.tmdb.org/t/p/original/';
   const defaultPoster =
     'https://upload.wikimedia.org/wikipedia/commons/f/f9/No-image-available.jpg';
   return (
     <>
       <MoviesItem>
-        <MovieLink to={`/movies/${id}`} state={{ from: location }}>
+        <MovieLink to={`${routes.MOVIES}/${id}`} state={{ from: location }}>
           <MoviePoster
             src={poster ? `${imageUrl}${poster}` : defaultPoster}
             alt={title || name}
